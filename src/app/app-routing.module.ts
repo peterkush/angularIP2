1b2ca7c56efcebacf4bf252dd0ed90207298bf08
peterkush <pkungu004@gmail.com>
2020-11-18 06:23:21 +0300

    added the nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from '../app/user/user.component'
+import { AboutComponent } from '../app/about/about.component'
+import { NotFoundComponent } from '../app/not-found/not-found.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <app-nav></app-nav>
   <app-user></app-user>
+  <app-about></app-about>
   `
 })
 export class AppComponent {
